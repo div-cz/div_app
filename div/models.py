7626a57491a9f823c005d2aefd9446f8c2732f62
div_cz <admin@div.cz>
2023-08-22 15:06:47 +0000

    settings for production

--- div/models.py
+++ div/models.py
@@ -1,46 +1,117 @@
+
 from django.db import models
 
-class Film(models.Model):
-    nazev = models.CharField(max_length=255, unique=True)
-    popis = models.TextField(blank=True, null=True)
-    datum_vydani = models.DateField(blank=True, null=True)
-
-    class Meta: 
-        app_label = 'divdb'
-
-    def __str__(self):
-        return self.nazev
-
-class Hra(models.Model):
-    nazev = models.CharField(max_length=255, unique=True)
-    popis = models.TextField(blank=True, null=True)
-    datum_vydani = models.DateField(blank=True, null=True)
+class MetaWorld(models.Model):
+    worldid = models.AutoField(primary_key=True)
 
     class Meta:
+        db_table = 'MetaWorld'
         app_label = 'divdb'
 
-    def __str__(self):
-        return self.nazev
-
-class Kniha(models.Model):
-    nazev = models.CharField(max_length=255, unique=True)
-    autor = models.CharField(max_length=255)
-    popis = models.TextField(blank=True, null=True)
-    datum_vydani = models.DateField(blank=True, null=True)
+class MetaGenre(models.Model):
+    genreid = models.AutoField(primary_key=True)
 
     class Meta:
+        db_table = 'MetaGenre'
+        app_label = 'divdb'
+
+class GamePlatform(models.Model):
+    platformid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'GamePlatform'
+        app_label = 'divdb'
+
+class GamePublisher(models.Model):
+    publisherid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'GamePublisher'
+        app_label = 'divdb'
+
+class GameDevelopers(models.Model):
+    developerid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'GameDevelopers'
+        app_label = 'divdb'
+
+class BookPublisher(models.Model):
+    publisherid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'BookPublisher'
+        app_label = 'divdb'
+
+class MetaCountry(models.Model):
+    countryid = models.AutoField(primary_key=True)
+
+    class Meta:
+        db_table = 'MetaCountry'
+        app_label = 'divdb'
+
+class Movie(models.Model):
+    movieid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    releaseyear = models.DateField()
+    duration = models.IntegerField()
+    language = models.CharField(max_length=5)
+    budget = models.DecimalField(max_digits=15, decimal_places=2)
+    world = models.ForeignKey(MetaWorld, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Movie'
         app_label = 'divdb'
 
     def __str__(self):
-        return self.nazev
+        return self.title
+
+class Game(models.Model):
+    gameid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    ratingid = models.IntegerField()
+    description = models.TextField()
+    platform = models.ForeignKey(GamePlatform, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(GamePublisher, on_delete=models.CASCADE)
+    genre = models.ForeignKey(MetaGenre, on_delete=models.CASCADE)
+    world = models.ForeignKey(MetaWorld, on_delete=models.CASCADE)
+    developer = models.ForeignKey(GameDevelopers, on_delete=models.CASCADE)
+    country = models.ForeignKey(MetaCountry, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Game'
+        app_label = 'divdb'
+
+    def __str__(self):
+        return self.title
+
+class Book(models.Model):
+    bookid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    genre = models.ForeignKey(MetaGenre, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(BookPublisher, on_delete=models.CASCADE)
+    world = models.ForeignKey(MetaWorld, on_delete=models.CASCADE)
+    country = models.ForeignKey(MetaCountry, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Book'
+        app_label = 'divdb'
+
+    def __str__(self):
+        return self.title
 
 class Lokalita(models.Model):
-    nazev = models.CharField(max_length=255, unique=True)
-    popis = models.TextField(blank=True, null=True)
-    adresa = models.TextField()
+    nazev = models.CharField(max_length=255, verbose_name="Název lokality")
+    popis = models.TextField(verbose_name="Popis lokality")
+    adresa = models.CharField(max_length=255, verbose_name="Adresa lokality")
 
     class Meta:
-        app_label = 'divdb'
+        db_table = 'Lokalita'
+        verbose_name = "Lokalita"
+        verbose_name_plural = "Lokality"
 
     def __str__(self):
         return self.nazev
--- div/views.py
+++ div/views.py
@@ -1,28 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
-from .models import Film, Hra, Kniha, Lokalita
+from .models import Movie, Game, Book, Lokalita
 from django.shortcuts import render
+
+
+def testtest(request):
+    context = {
+        'title': 'Testovaci titulek',
+        'content': 'Testovaci obsah stranky'
+        }
+    return render(request, 'test.html', context)
+
+def filmy_z_roku_2000(request):
+    filmy = Film.objects.filter(rok=2000)
+    return render(request, 'filmy.html', {'filmy': filmy})
 
 def index(request):
 #        return HttpResponse("Toto je hlavní stránka.kontakt")
         return render(request, 'index.html')
 
 def filmy(request):
-        filmy = Film.objects.all()
-        return render(request, 'filmy/filmy_list.html', {'filmy': filmy})
+        return HttpResponse("hlavni strana fff")
+#        filmy = Movie.objects.all()
+#        return render(request, 'filmy/filmy_list.html', {'filmy': filmy})
 #        return HttpResponse("hlavni strana fff")
 
 def film_detail(request, nazev_filmu):
-    film = get_object_or_404(Film, nazev=nazev_filmu)
+    film = get_object_or_404(Movie, title=nazev_filmu)
     return render(request, 'filmy/film_detail.html', {'film': film})
 
 # Pro hry
 def hry(request):
-    hry = Hra.objects.all()
+    hry = Game.objects.all()
     return render(request, 'hry/hry_list.html', {'hry': hry})
 
 def hra_detail(request, nazev_hry):
-    hra = get_object_or_404(Hra, nazev=nazev_hry)
+    hra = get_object_or_404(Game, nazev=nazev_hry)
     return render(request, 'hry/hra_detail.html', {'hra': hra})
 
 # Pro knihy
