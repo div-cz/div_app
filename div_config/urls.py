95e155c87da5851bd7a48766d7f4393c24cdbe24
herodotos <michalikjuraj0@gmail.com>
2024-02-08 18:05:52 +0100

    Rename api to div_api

--- div_config/urls.py
+++ div_config/urls.py
@@ -35,7 +35,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/', include('div_api.urls')),
 ]
 
 
