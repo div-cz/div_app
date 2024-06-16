267903474734365ab6afeee1df3395b3ec07190a
div_cz <admin@div.cz>
2024-06-16 05:56:04 +0000

    latest version 16.06.2024

--- div_content/forms/games.py
+++ div_content/forms/games.py
@@ -4,15 +4,55 @@ from django import forms
 from div_content.models import Game, Gamedevelopers, Gameplatform, Gamepublisher, Metacountry, Metagenre, Metauniversum
 
 class GameForm(forms.ModelForm):
-    developerid = forms.ModelChoiceField(queryset=Gamedevelopers.objects.all()[:4], label="Developer")
-    platformid = forms.ModelChoiceField(queryset=Gameplatform.objects.all()[:4], label="Platform")
-    publisherid = forms.ModelChoiceField(queryset=Gamepublisher.objects.all()[:4], label="Publisher")
-    countryid = forms.ModelChoiceField(queryset=Metacountry.objects.all()[:4], label="Country")
-    universumid = forms.ModelChoiceField(queryset=Metauniversum.objects.all()[:4], label="Universum")
-    genreid = forms.ModelChoiceField(queryset=Metagenre.objects.all()[:4], label="Genre")
+    developerid = forms.ModelChoiceField(
+        queryset=Gamedevelopers.objects.all(),
+        label="Developer",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+    platformid = forms.ModelChoiceField(
+        queryset=Gameplatform.objects.all(),
+        label="Platform",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+    publisherid = forms.ModelChoiceField(
+        queryset=Gamepublisher.objects.all(),
+        label="Publisher",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+    countryid = forms.ModelChoiceField(
+        queryset=Metacountry.objects.all(),
+        label="Country",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+    universumid = forms.ModelChoiceField(
+        queryset=Metauniversum.objects.all(),
+        label="Universum",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+    genreid = forms.ModelChoiceField(
+        queryset=Metagenre.objects.all(),
+        label="Genre",
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False,
+        empty_label="--- Vyberte ---"
+    )
+
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+        required=False,
+        label="Description"
+    )
 
     class Meta:
         model = Game
         fields = ['title', 'description', 'developerid', 'platformid', 'publisherid', 'countryid', 'universumid', 'genreid']
-
-
