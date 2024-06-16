7b43feb18d6abb9e4b0ac8269749c2a3fda1a9b2
Martin Kučera <158811340+KuceraMar@users.noreply.github.com>
2024-06-16 20:11:23 +0200

    Update locations.py

--- div_content/views/locations.py
+++ div_content/views/locations.py
@@ -1,16 +1,18 @@
 # VIEWS.LOCATIONS.PY
 
 from django.shortcuts import get_object_or_404, render
-from div_content.models import Location
+from div_content.models import Metacity, Metalocation
 
 
 
 def locations(request):
-    locations = Location.objects.all()
-    return render(request, 'meta/locations_list.html', {'locations': locations})
+    locations = Metalocation.objects.all()[:10]
+    cities = Metacity.objects.all()[:10]
+    
+    return render(request, 'meta/locations_list.html', {'locations': locations, 'cities': cities})
 
 def location_detail(request, location_url):
-    location = get_object_or_404(Location, url=location_url)
+    location = get_object_or_404(Metalocation, url=location_url)
     return render(request, 'meta/location_detail.html', {'location': location})
 
 
