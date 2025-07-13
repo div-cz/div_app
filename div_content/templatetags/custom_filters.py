65111cfd35a2c7389d158673bba010e4c2aa7bc6
Kuceramar <martin@div.cz>
2025-07-13 12:27:24 +0000

    jednotn cena

--- div_content/templatetags/custom_filters.py
+++ div_content/templatetags/custom_filters.py
@@ -8,7 +8,7 @@ def price_format(value):
     if value is None:
         return "0"
     try:
-        locale.setlocale(locale.LC_ALL, 'cs_CZ.UTF-8')
+        # locale.setlocale(locale.LC_ALL, 'cs_CZ.UTF-8')
         return locale.format_string("%.0f", float(value), grouping=True)
     except:
         return str(value)
