3dc892ac2cfe3213ff9a61d17a7a6e48e5662e68
Kuceramar <martin@div.cz>
2025-07-28 14:40:23 +0000

    books

--- div_config/urls.py
+++ div_config/urls.py
@@ -1,14 +1,3 @@
-""" URLS.PY TEST
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 # -------------------------------------------------------------------
 #                    URLS.PY
 # -------------------------------------------------------------------
